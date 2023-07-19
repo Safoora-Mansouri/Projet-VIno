ffcb3cafcc97180629d1e313f6e162cb7e7571cf
safoora <safoura.mansouri@gmail.com>
2023-07-19 08:45:09 -0400

    modify addCellar.php

--- app/Http/Livewire/AddCellar.php
+++ app/Http/Livewire/AddCellar.php
@@ -23,8 +23,9 @@ class AddCellar extends Component
     public function store()
     {
         $this->validate();
-        $userId=2;
-        // $userId = Auth::check() ? Auth::id() : null;
+         //$userId=2;
+         $userId = Auth::check() ? Auth::id() : null;
+        
 
         Cellar::create([
             'name' => $this->nom,
